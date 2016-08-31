c0bee2d294a607508f80a029fd0cc4cb8f084503
caiopo <caiopoliveira@gmail.com>
2016-08-31 10:07:36 -0300

    Improve Main

--- GerenciadorDeEventos/src/main/Main.java
+++ GerenciadorDeEventos/src/main/Main.java
@@ -2,58 +2,73 @@ package main;
 
 import java.util.Scanner;
 
-import model.curso.Curso;
 import model.curso.Evento;
-import model.pessoa.Pessoa;
+import model.curso.Minicurso;
+import model.curso.Palestra;
+import model.pessoa.Palestrante;
+import model.pessoa.Participante;
 import view.CadastroCurso;
 import view.CadastroEvento;
 import view.CadastroPessoa;
+import view.EditaCurso;
+import view.EditaPessoa;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner entrada = new Scanner(System.in);
 		int opcao;
 		boolean on = true;
-		Evento evento = new CadastroEvento().cadastroEvento();
-		CadastroCurso cadastroCurso = new CadastroCurso();
-		
-		while(on) {
-	        System.out.println("Informe a opção desejada:");
-	        System.out.println("1 - Cadastrar Participante");
-	        System.out.println("2 - Cadastrar Palestrante");
-	        System.out.println("3 - Cadastrar Minicurso");
-	        System.out.println("4 - Cadastrar Palestra");
-	        System.out.println("5 - Sair");
-	        opcao = entrada.nextInt();
-	        
+		Evento evento = CadastroEvento.cadastroEvento();
+
+		while (on) {
+			System.out.println("Informe a opção desejada:");
+			System.out.println("1 - Cadastrar Participante");
+			System.out.println("2 - Cadastrar Palestrante");
+			System.out.println("3 - Cadastrar Minicurso");
+			System.out.println("4 - Cadastrar Palestra");
+			System.out.println("5 - Sair");
+			opcao = entrada.nextInt();
+
 			switch (opcao) {
-				case 1:
-					Pessoa a = new CadastroPessoa().cadastroParticipante();
-					System.out.println(a.toString());
-					break;
-				case 2:
-					Pessoa b = new CadastroPessoa().cadastroPalestrante();
-					System.out.println(b.toString());
-					break;
-				case 3:
-					Curso c = cadastroCurso.cadastroMinicurso();
-					System.out.println(c.toString());
-					evento.adicionar(c);
-					break;
-				case 4:
-					Curso d = cadastroCurso.cadastroPalestra();
-					System.out.println(d.toString());
-					evento.adicionar(d);
-					break;
-				case 5:
-					on = false;
-					break;
-				default:
-					System.out.println("Opção inválida");
-					break;
+			case 1:
+				Participante a = CadastroPessoa.cadastroParticipante();
+				System.out.println(a.toString());
+				EditaPessoa.editarParticipante(a);
+				System.out.println(a.toString());
+				break;
+			case 2:
+				Palestrante b = CadastroPessoa.cadastroPalestrante();
+				System.out.println(b.toString());
+				EditaPessoa.editarPalestrante(b);
+				System.out.println(b.toString());
+				break;
+			case 3:
+				Minicurso c = CadastroCurso.cadastroMinicurso();
+				System.out.println(c.toString());
+				EditaCurso.edita(c);
+				System.out.println(c.toString());
+				evento.adicionar(c);
+				break;
+			case 4:
+				Palestra d = CadastroCurso.cadastroPalestra();
+				System.out.println(d.toString());
+				EditaCurso.edita(d);
+				System.out.println(d.toString());
+				evento.adicionar(d);
+				break;
+			case 5:
+				on = false;
+				break;
+			default:
+				System.out.println("Opção inválida");
+				break;
 			}
+
 			entrada.nextLine();
 		}
+
+		entrada.close();
 	}
 }
